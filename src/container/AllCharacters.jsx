6c4be6ec57c6fa40c935077326703a1d5c24cf1b
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-08 16:25:53 -0500

    add router and update hierarchy

--- src/container/AllCharacters.jsx
+++ src/container/AllCharacters.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import CharacterList from '../components/characters/CharacterList';
-import Detail from '../components/detail/Detail';
 
 export default class AllCharacters extends Component {
     static propTypes = {
@@ -15,7 +14,6 @@ export default class AllCharacters extends Component {
             Hello from AllCharacters container
           </div>
           <CharacterList />
-          <Detail />
         </>
       );
     }
