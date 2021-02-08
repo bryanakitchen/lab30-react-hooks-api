a00eb12c8ceb993b78143820d9676027d3cbc591
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-08 16:35:40 -0500

    create detail page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import AllCharacters from '../../container/AllCharacters';
 import Detail from '../detail/Detail';
 import Header from '../header/Header';
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Detail() {
+function Detail({ name, url }) {
   return (
-    <div>
-        Hello from Detail
-    </div>
+    <>
+      <figure>
+        <img src={url} />
+        <figcaption>{name}</figcaption>
+      </figure>
+        
+    </>
   );
 }
 
 Detail.propTypes = {
-
+  name: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
 };
 
 export default Detail;
