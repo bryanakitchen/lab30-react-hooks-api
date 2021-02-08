9a324516a1f3261d7fa8734f9b17af8c9e83d771
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-08 17:10:50 -0500

    fill out characterList

--- src/components/characters/CharacterList.jsx
+++ src/components/characters/CharacterList.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Character from './Character';
 
-function CharacterList() {
-  return (
-    <>
-      <div>
-        Hello from CharacterList
-      </div>
-      <Character />
-    </>
-  );
+function CharacterList({ characters }) {
+  
+  // destructure?
+  const characterElements = characters.map(character => {
+    <li key={character._id} >
+      <Character name={character.name} url={character.url}/>
+    </li>;
+  });
+
+  return <ul>{characterElements}</ul>;
 }
 
 CharacterList.propTypes = {
-
+  characters: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired
+    })
+  )
 };
 
 export default CharacterList;
